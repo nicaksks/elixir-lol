d35984d0232e8fcb45faa26658a0eaab2ddeffc1
nica <100009274+nicaksks@users.noreply.github.com>
2024-01-11 03:03:01 -0300

    Update json.ex

--- backend/lib/widget/utils/json.ex
+++ backend/lib/widget/utils/json.ex
@@ -2,7 +2,7 @@ defmodule Api.Json do
 
   import Plug.Conn
 
-  def send(conn, data \\ %{}, status) do
+  def res(conn, data \\ %{}, status) do
     conn
     |> put_resp_content_type("application/json")
     |> send_resp(status, Jason.encode!(data))
